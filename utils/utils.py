a0ff5b686e5509ed56602eb69653dfbe9618209f
diaa1999abujaber@gmail.com <diaa1999abujaber@gmail.com>
2020-12-20 02:25:09 +0200

    Add dataframe generator code to utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,11 @@
+import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
  
 plt.style.use("ggplot")
+
 
 def split_data(df):
 	X_train, X_test, y_train, y_test = train_test_split(df['id'], df['label'], test_size = 0.2)
@@ -12,6 +14,15 @@ def split_data(df):
 	df_test = pd.concat((X_test, y_test), axis = 1)	
 
 	return df_train, df_test
+
+def generate_csv(path):
+  print('[INFO]::Generating images dataframe...')
+  images = glob.glob(path)
+  df = pd.DataFrame({'id': images})
+  df['label'] = 0
+  df['label'] = df['id'].apply(lambda x: x.split('/')[4])
+
+  return df
 
 def plot_learning_curve(H, EPOCHS):
 
@@ -23,4 +34,13 @@ def plot_learning_curve(H, EPOCHS):
   plt.xlabel("Epoch #")
   plt.ylabel("Loss")
   plt.legend(loc="lower left")
-  plt.savefig('plots/6-layers-cnn-lr_curve')
+  plt.savefig('eff-net-lr_curve')
+
+def plot_hist(hist):
+    plt.plot(hist.history["accuracy"])
+    plt.plot(hist.history["val_accuracy"])
+    plt.title("model accuracy")
+    plt.ylabel("accuracy")
+    plt.xlabel("epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
