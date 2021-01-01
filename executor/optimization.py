e394f8431ac2e61f20c2fa726df94463e92998ed
diaa1999abujaber@gmail.com <diaa1999abujaber@gmail.com>
2021-01-01 17:57:23 +0200

    Implement optimization decorator

---
+++ executor/optimization.py
@@ -0,0 +1,43 @@
+import optuna
+from configs.config import CFG
+
+class optimize:
+  """
+  Decorator class for automating hyperparamters tuning using OpTuna
+  """
+  
+  def __init__(self, n_trials = 5, direction='minimize'):  
+    self.n_trials = n_trials
+    self.direction = direction
+    self.executor = None
+        
+  def __call__(self, executor):
+    self.executor = executor
+    
+    def wrapper(*args, **kwargs):
+      self.study = optuna.create_study(direction=self.direction)
+      return self.study.optimize(self._objective, n_trials=self.n_trials)
+    
+    return wrapper
+        
+  def _objective(self, trail):
+    params = {
+      'shape': CFG['shape'],
+      'batch_size': CFG['batch_size'],
+      'epochs': CFG['epochs'],
+      
+      'optimizer': trail.suggest_categorical('optimizer', CFG['optimizer']),
+      
+      'learning_rate': trail.suggest_loguniform(
+          'learning_rate', CFG['learning_rate'][0], CFG['learning_rate'][1]),
+    
+      'training_aug': CFG['training_aug'],
+      'test_aug': CFG['test_aug'],
+      
+      'fine-tune': trail.suggest_categorical('fine-tune', CFG['fine-tune'])
+    }
+    print(params)
+    loss = self.executor(params)
+    return loss
+  
+  
