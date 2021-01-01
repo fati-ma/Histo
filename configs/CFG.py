0afee1ad7d674f4d56b3b175cc93d62c06a1468c
diaa1999abujaber@gmail.com <diaa1999abujaber@gmail.com>
2021-01-01 13:18:02 +0200

    Clean the branch from unrelated files

--- configs/CFG.py
+++
@@ -1,9 +0,0 @@
-base_path="IDC_regular_ps50_idx5/part1"
-data_size= 157570
-Augm_size= 78786
-train_size= 0.7
-val_size= 0.15
-test_size= 0.15
-img_dim= (50,50,3)
-batch_size=128
-learning_rate=0.01
